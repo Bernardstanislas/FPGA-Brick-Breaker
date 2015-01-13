5f5abecdcf59bae5dd99a61d2490ca923b037433
Stanislas Bernard <bernardstanislas@gmail.com>
2015-01-13 12:23:45 +0100

    TB for BBox

--- brick-breaker.srcs/sim_1/new/TB_BBox.vhd
+++ brick-breaker.srcs/sim_1/new/TB_BBox.vhd
@@ -36,8 +36,65 @@ entity TB_BBox is
 end TB_BBox;
 
 architecture Behavioral of TB_BBox is
+    component BBox is
+    port (
+        clock        :    in  std_logic;
+        triggerSet  :   in  std_logic;
+    
+        setX        :   in  integer;
+        setY        :   in  integer;
+        getX        :   out integer;
+        getY        :   out integer;
+        
+        setWidth    :     in     integer;
+        setHeight     :     in     integer;
+        getWidth    :    out    integer;
+        getHeight    :    out integer;
+        
+        setAlive    :   in  std_logic;
+        getAlive    :   out std_logic
+    );
+    end component;
+    
+    signal triggerSet : std_logic := '0';
+    signal setX : integer := 500;
+    signal setY : integer := 500;
+    signal getX : integer := 0;
+    signal getY : integer := 0;
+    signal setWidth : integer := 30;
+    signal setHeight : integer := 10;
+    signal getWidth : integer := 0;
+    signal getHeight : integer := 0;
+    
+    signal setAlive : std_logic := '1';
+    signal getAlive : std_logic;
 
+    signal CLK : std_logic := '0';
+    constant CLK_period : time := 10 ns;
 begin
+    uut : BBox port map (clk, triggerSet, setX, setY, getX, getY, setWidth, setHeight, getWidth, getHeight, setAlive, getAlive);
 
+    CLK_process : process
+            begin
+                CLK <= '0';
+                wait for CLK_period/2;
+                CLK <= '1';
+                wait for CLK_period/2;
+    end process;
+    
+    stim_proc : process
+    begin
+        wait for CLK_period * 2;
+        triggerSet <= '1';
+        wait for CLK_period;
+        triggerSet <= '0';
+        wait for CLK_period * 3;
+        setX <= 666;
+        setY <= 90;
+        triggerSet <= '1';
+        wait for CLK_period;
+        triggerSet <= '0';
+        wait;
+    end process;
 
 end Behavioral;
